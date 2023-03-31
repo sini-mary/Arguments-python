7249fac0e4352689c0242a6997dd0ab7dfeb6f0f
sini-mary <marysnaidaa@gmail.com>
2023-03-31 15:57:24 +0300

    first commit

---
+++ math.py
@@ -0,0 +1,15 @@
+def subtract(num1,num2):
+    subtraction =num1-num2
+    return subtraction
+
+def divide(x,y):
+    division = x/y
+    return division
+
+def multiply(e,f):
+    multiplication=e*f
+    return multiplication
+
+def remainder(w,x):
+    modulus= w%x
+    return modulus
